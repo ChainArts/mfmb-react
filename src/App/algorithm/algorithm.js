f646ed8fae6c01108e669da77d53edf1148ef519
Martin <platajs.martin@gmail.com>
2020-09-24 18:41:05 +0200

    updated algorithm

--- src/App/algorithm/algorithm.js
+++ src/App/algorithm/algorithm.js
@@ -1,5 +1,10 @@
 var mysql = require('mysql');
 var d = new Date();
+var update = []; 
+var geld = [];
+var points = [];
+var i=0;
+
 
 var con = mysql.createConnection({
     host: "localhost",
@@ -12,12 +17,29 @@ con.connect(function(err) {
     if (err) throw err;
     console.log("Connected");
 
+
     con.query("SELECT * FROM firmen", function (err, result, fields) {
         if (err) throw err;
-        console.log(result[0].update_time);
-        console.log(d);
+        
+        // function iterate(number) {
+        //     console.log(number)
+        // }
+
+
+        result.forEach(number=>{
+            geld[i] = number.geld;
+            update[i] = number.update_time;
+            c=d-update[i];
+            
+            i++
+        });
     });
-
-
 });
 
+
+
+
+
+
+
+ 
