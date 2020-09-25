1d726e86dbc6a5609ad66b4bad3544f1bdeed0d9
Martin <platajs.martin@gmail.com>
2020-09-25 07:25:39 +0200

    convert js into ts (typescript)

--- src/App/algorithm/algorithm.ts
+++ src/App/algorithm/algorithm.ts
@@ -1,30 +1,45 @@
 var mysql = require('mysql');
 var d = new Date();
-var update = [];
+var update = []; 
 var geld = [];
 var points = [];
-var i = 0;
+var i=0;
+
+
+
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
     database: "mfmb"
 });
-con.connect(function (err) {
-    if (err)
-        throw err;
+
+con.connect(function(err) {
+    if (err) throw err;
     console.log("Connected");
+
+
     con.query("SELECT * FROM firmen", function (err, result, fields) {
-        if (err)
-            throw err;
+        if (err) throw err;
+        
         // function iterate(number) {
         //     console.log(number)
         // }
-        result.forEach(function (number) {
+
+
+        result.forEach(number=>{
             geld[i] = number.geld;
             update[i] = number.update_time;
             //c=d-update[i];                need to get update times from database to calculate score
-            i++;
+            i++
         });
     });
 });
+
+
+
+
+
+
+
+ 
