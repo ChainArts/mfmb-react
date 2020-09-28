cba8f0d49da44c34174aefae5bc46a625b3046a4
Martin <platajs.martin@gmail.com>
2020-09-28 14:53:00 +0200

    added timestamp, money and noise calc

--- src/App/algorithm/algorithm.ts
+++ src/App/algorithm/algorithm.ts
@@ -11,24 +11,25 @@ con.connect(function(err) {
     if (err) throw err;
     console.log("Connected");
 
-
     con.query("SELECT * FROM firmen", function (err, result, fields) {
         if (err) throw err;
-        var d = new Date(2020,8,25,9,5,30,0);
-        var update = [];
         
+        var d = new Date(2020,8,25,9,5,30,0);
+        var update = [], money = [], actuality: Array<number> = [];
+        var i = 0, noise = 0;
 
-
-        result.forEach(number=>{
-
-            var i = 0;
-            update[i] = number.update;
-            console.log(update[i]);
-
+        result.forEach(company=>{
+            money.push(company.geld)
+            update.push(company.update);
+            actuality.push(d.getTime() - update[i]);
             i++;
         });
-        console.log(d);
-        console.log(d.getTime()-update[0]);
+
+        noise = Math.round(Math.random()*money.length)
+        console.log(update);
+        console.log(money);
+        console.log(actuality);
+        console.log(noise,money.length);
 
     });
 });
