794cae4832d30e2edbe69c60d3750bb9971a2541
Martin <platajs.martin@gmail.com>
2020-10-02 01:14:47 +0200

    changed to object oriented programming

--- src/App/algorithm/algorithm.ts
+++ src/App/algorithm/algorithm.ts
@@ -1,5 +1,32 @@
-var mysql = require('mysql');
+var d = new Date(2020, 8, 25, 9, 5, 30, 0);
+var playtime = [0, 10, 20, 30, 5, 15, 20, 25, 17, 13, 10, 9, 22];
+var companies = [];
+var i = 0;
 
+function Company(id, name, geld, update, playtime) {
+    this.id = id;
+    this.name = name;
+    this.geld = geld;
+    this.update = update;
+    this.actuality = d.getTime() - this.update;
+    this.playtime = playtime;
+    this.displaytime = 0;
+
+    this.get_displaytime = function() {
+        return this.displaytime;
+    }
+
+    this.set_displaytime = function(time) {
+        this.displaytime = time;
+    }
+}
+
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+
+var mysql = require('mysql');
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -7,32 +34,37 @@ var con = mysql.createConnection({
     database: "mfmb"
 });
 
-con.connect(function(err) {
-    if (err) throw err;
+con.connect(function (err) {
+    if (err)
+        throw err;
     console.log("Connected");
-
     con.query("SELECT * FROM firmen", function (err, result, fields) {
-        if (err) throw err;
-        
-        var d = new Date(2020,8,25,9,5,30,0);
-        var update = [], money = [], actuality: Array<number> = [];
-        var i = 0, noise = 0;
+        if (err) {
+            throw err;
+        }
 
-        result.forEach(company=>{
-            money.push(company.geld)
-            update.push(company.update);
-            actuality.push(d.getTime() - update[i]);
+        result.forEach(function (company) {
             i++;
+            companies [i] = new Company(company.id,company.name,company.geld,company.update,playtime[i]);
         });
+        
+        companies.sort((a, b) => a.playtime - b.playtime);
+        console.log(companies);
 
-        noise = Math.round(Math.random()*money.length)
-        console.log(update);
-        console.log(money);
-        console.log(actuality);
-        console.log(noise,money.length);
 
+
+
+        //>id=Math.round(Math.random() * money.length);
+        //noise = id/2;
+        // console.log(update);
+        // console.log(money);
+        // console.log(actuality);
+        // console.log(noise, id, money.length);
     });
+
 });
+
+
 
 
 
