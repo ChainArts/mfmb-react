1a38c46ecd20a5871e53cc5b7919fd45782c026a
Martin Platajs <Platajs.Martin@gmail.com>
2020-10-04 20:01:47 +0200

    kinda working algorithm

--- src/App/algorithm/algorithm.js
+++ src/App/algorithm/algorithm.js
@@ -1,7 +1,7 @@
 var d = new Date(2020, 8, 25, 9, 5, 30, 0);
 var playtime = [0, 10, 20, 30, 5, 15, 20, 25, 17, 13, 10, 9, 22];
 var companies = [];
-var i = 0;
+var i = 0, anz = 0, rep = 0, debug = 0;
 function Company(id, name, geld, update, playtime) {
     this.id = id;
     this.name = name;
@@ -39,8 +39,37 @@ con.connect(function (err) {
             i++;
             companies[i] = new Company(company.id, company.name, company.geld, company.update, playtime[i]);
         });
-        companies.sort(function (a, b) { return a.playtime - b.playtime; });
-        console.log(companies);
+        //companies.sort((a, b) => a.playtime - b.playtime);
+        companies.sort(function (a, b) { return a.id - b.id; });
+        //console.log(companies);
+        var id = 0, prev_id = 0;
+        var selection = [];
+        while (rep <= 24) {
+            companies.sort(function (a, b) { return a.displaytime - b.displaytime; });
+            selection = [0];
+            for (i = 0; i <= 11; i++) {
+                if (companies[0].displaytime == companies[i].displaytime) {
+                    //console.log(companies[0].displaytime, companies[i].displaytime);
+                    //console.log(companies[i].id,);
+                    selection.push(companies[i].id);
+                }
+            }
+            console.log(companies.map(function (a) { return a.id; }));
+            console.log(selection);
+            companies.sort(function (a, b) { return a.id - b.id; });
+            while (id == prev_id) {
+                debug = random(0, selection.length - 2);
+                console.log(debug);
+                id = selection[debug];
+                console.log(id + 1, selection.length - 1);
+            }
+            rep++;
+            companies[id].displaytime += companies[id].playtime;
+            //console.log(companies[id].id, companies[id].displaytime);
+            prev_id = id;
+        }
+        console.log(companies.map(function (a) { return a.id; }));
+        console.log(companies.map(function (a) { return a.displaytime; }));
         //>id=Math.round(Math.random() * money.length);
         //noise = id/2;
         // console.log(update);
