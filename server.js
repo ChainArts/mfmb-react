f0ecc01986cdc1c98b300def09dfa2e20f76d0d1
Maximilian Roll <maxroll54@gmail.com>
2020-12-17 19:14:23 +0100

    added webserver files

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var http = require('http');
+
+//var algorithm = require('./webserver/algorithm/algorithm')
+
+const PORT = 5500;
+
 http.createServer(function (req, res) {
     res.write('Response from NodeJS Server');
     res.end();
-}).listen(5500);
+}).listen(PORT);
 
-console.log("Server started at localhost:5500");
+console.log("Server started at localhost:" + PORT);
