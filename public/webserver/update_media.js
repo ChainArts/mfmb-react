03e45705cf9734e5c61c0f2eb76c8d74ed81bb57
Martin <platajs.martin@gmail.com>
2021-02-24 20:56:31 +0100

    file update done (paths still need to be updated)

--- public/webserver/update_media.js
+++ public/webserver/update_media.js
@@ -1,13 +1,21 @@
 const klawSync = require('klaw-sync');
-const through2 = require('through2');
 const fs = require('fs-extra');
 const homedir = require('os').homedir();
-var server_paths = [];
+const { getVideoDurationInSeconds } = require('get-video-duration');
 
-const client_dir = homedir + "\\Documents\\";
-
-
-const server_dir = klawSync('\\\\192.168.8.13\\Share\\MFMB', {nofile: true});
+function File(path,mtime,fname,type,campaign,company) {
+  this.path = path;
+  this.mtime = mtime;
+  this.fname = fname;
+  this.type = type;
+  this.campaign = campaign;
+  this.company = company;
+  this.getcontentLength = function(){
+    getVideoDurationInSeconds(client_DocDir + this.path).then((duration) => {
+      console.log(duration)
+    })
+  };
+}
 
 function arr_diff (array1, array2) {
   var array = [], diff = [];
@@ -28,70 +36,141 @@ function arr_diff (array1, array2) {
   return diff;
 }
 
+function removeDeletedData (){
+  var diff = arr_diff(serverFiles.map(obj => obj.path),clientFiles.map(obj => obj.path));
+  diff.forEach(item =>{
+    console.log(client_DocDir + item);
+    fs.removeSync(client_DocDir + item);
+  });
+}
+
+function removeFilename (path){
+  var buffer = path.split('/');
+  buffer.pop();
+  path = buffer.join('/');
+  return path;
+}
+
 function format (obj){
-  var buffer = obj.path.split('\\')
-  while(buffer[0] != 'MFMB'){
+  var buffer = obj.path.split('\\');
+  while(buffer[0] != 'Companies'){
     buffer.shift();
     }
     return buffer.join('/');
 }
 
+function getInfo (obj,i){                   //i: 0 = Error; i: 1 = Filename; i: 2 = media-type; i: 3 = Campaign number; i: 4 = Company Name
+  if(0<i<5){
+    var buffer = format(obj).split('/');
+    return buffer[buffer.length - i];
+  }else{
+    return "error";
+  }
 
+}
+
+var server_DocDir = '\\\\192.168.8.13\\Share\\MFMB\\'
+var server_DocDir = server_DocDir.split('\\').join('/');
+var client_DocDir = homedir + "\\Documents\\MFMB\\";
+var client_DocDir = client_DocDir.split('\\').join('/');
+
+const server = klawSync(server_DocDir, {nodir: true});
+var serverFiles = [];
+server.forEach(function (obj, index){
+  serverFiles[index] = new File(format(obj),obj.stats.birthtime,getInfo(obj,1),getInfo(obj,2),getInfo(obj,3),getInfo(obj,4));
+});
+
+serverFiles.forEach(item =>{
+  fs.ensureDirSync(client_DocDir + removeFilename(item.path));
+  var files = fs.readdirSync(client_DocDir + removeFilename(item.path));
+  var length = files.length
+  if(!fs.pathExistsSync(client_DocDir + item.path) && length == 0){
+    fs.copySync(server_DocDir + item.path,client_DocDir + item.path);
+  }
+});
+
+
+const client = klawSync(client_DocDir, {nodir: true});
+var clientFiles = [];
+client.forEach(function (obj, index){
+  clientFiles[index] = new File(format(obj),obj.stats.birthtime,getInfo(obj,1),getInfo(obj,2),getInfo(obj,3),getInfo(obj,4));
+});
+
+
+serverFiles.forEach(item =>{
+  if(item.mtime > clientFiles[clientFiles.findIndex(function(file){ return removeFilename(item.path) == removeFilename(file.path)})].mtime){
+    console.log(item);
+    console.log(clientFiles[clientFiles.findIndex(function(file){ return removeFilename(item.path) == removeFilename(file.path)})]);
+    fs.copySync(server_DocDir + item.path, client_DocDir + removeFilename(item.path) + "/buffer-" + item.fname);
+    fs.removeSync(client_DocDir + clientFiles[clientFiles.findIndex(function(file){ return removeFilename(item.path) == removeFilename(file.path)})].path);
+    fs.renameSync(client_DocDir + removeFilename(item.path) + "/buffer-" + item.fname, client_DocDir + item.path)
+  }
+});
+
+
+const client_new = klawSync(client_DocDir, {nodir: true});
+var clientFiles = [];
+client_new.forEach(function (obj, index){
+  clientFiles[index] = new File(format(obj),obj.stats.birthtime,getInfo(obj,1),getInfo(obj,2),getInfo(obj,3),getInfo(obj,4));
+});
+
+clientFiles.forEach(item =>{
+  if(item.type == 'video'){
+    item.getcontentLength();
+  }
+
+})
+
+//removeDeletedData();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function removeDeletedData (server_dir,client_DocDir){
+  const client_dir = klawSync(client_DocDir + 'MFMB', {nofile: true});
+  var diff = arr_diff(server_dir.map(obj => format(obj)),client_dir.map(obj => format(obj)));
+  diff.forEach(element =>{
+    console.log(client_DocDir + element);
+    fs.removeSync(client_DocDir + element);
+  });
+}
+
+
+
+var server_paths = [];
+
+
+console.log(client_DocDir);
+
+var server_dir = klawSync('\\\\192.168.8.13\\Share\\MFMB', {nofile: true});
 
 
 var i =0;
 server_dir.forEach(function (item){
   if(item.path.includes('video') || item.path.includes('image')){
-    pathBuffer = server_dir[i].path.split('\\');
-    while(pathBuffer[0] != 'MFMB'){
-      pathBuffer.shift();
-      }
-      server_paths = pathBuffer.join('/');
-      pathBuffer = [];
-      //console.log(client_dir + server_paths);
-      fs.ensureDirSync(client_dir + server_paths);
+      server_paths = format(server_dir[i]);
+      console.log(client_DocDir + server_paths);
+      fs.ensureDirSync(client_DocDir + server_paths);
   }
     i++;
 });
 
-const client_dirs = klawSync(client_dir + 'MFMB', {nofile: true});
-var diff = arr_diff(server_dir.map(obj => format(obj)),client_dirs.map(obj => format(obj)));
-diff.forEach(element =>{
-  fs.removeSync(client_dir + element);
-});
+removeDeletedData(server_dir,client_DocDir);
 
-
-
-/*
-i=0;
-server_dir.forEach(function (dir) {
-    fs.ensureDirSync(dir.path);
-});
-*/
-/*
-const excludeDirFilter = through2.obj(function (item, enc, next) {
-  if (!item.stats.isDirectory()) this.push(item)
-  next()
-})
-
-const dirs = klawSync('\\\\192.168.8.13\\Share', {nofile: true});
-const otherdirs = klawSync('./Client', {nofile: true});
-
-  console.log(dirs);
-  console.log(otherdirs);
-  console.log(dirs[0].stats.ctime);
-  console.log(otherdirs[0].stats.ctime);
-  console.log(dirs[0].stats.ctime-otherdirs[0].stats.ctime);
-  
-  
-  if((dirs[0].stats.ctime-otherdirs[0].stats.ctime) > 0){
-      fs.remove(otherdirs[0].path);
-      fs.copy(dirs[0].path,otherdirs[0].path);
-      console.log("updated");
-  }
-  else{
-    console.log("already up to date");
-  }
 */
 
 
